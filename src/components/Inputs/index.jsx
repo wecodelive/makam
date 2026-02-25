7bfc451c97c9bdbe69a05c4c1bf10c8b8efae9c4
alatise001 <76849022+alatise001@users.noreply.github.com>
2026-02-25 10:05:33 +0100

    Update index.jsx

--- src/components/Inputs/index.jsx
+++ src/components/Inputs/index.jsx
@@ -1,4 +1,4 @@
 export { default as Search } from "./Search";
 export { default as Input } from "./Input";
-export { default as Checkbox } from "./Checkbox";
+export { default as Checkbox } from "./CheckBox";
 export { default as MultipleCheckBoxes } from "./MultipleCheckBoxes";
