8461f56b048e44b2827ca9340fe70ee2ed34fab9
alatise001 <76849022+alatise001@users.noreply.github.com>
2026-03-22 19:11:49 +0100

    Fix CommonJS/ESM conflict: use .cjs extension for serverless handler

---
+++ api/handler.cjs
@@ -0,0 +1,8 @@
+const app = require('../server/app.cjs');
+
+module.exports = function handler(req, res) {
+  // Remove /api prefix from the request URL for the Express app
+  const url = req.url.replace(/^\/api/, '') || '/';
+  req.url = url;
+  return app(req, res);
+};
